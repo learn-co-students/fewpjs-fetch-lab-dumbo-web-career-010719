0a37a82ab88ebd533547d7b3c75ac1afc8edaaf6
rchow1944 <rchow2@buffalo.edu>
2019-02-21 10:20:51 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,36 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+}
+
+function renderBooks(json) {
+  const booksList = document.getElementById("books")
+  console.log(json);
+  for (book of json) {
+    renderBook(book);
+  }
+}
+
+function renderBook(bookData) {
+  const booksList = document.getElementById("books")
+  const newBookEl = document.createElement('li');
+  const titleEl = document.createElement('h2');
+  const authorEl = document.createElement('p');
+  const pagesEl = document.createElement('p');
+
+  // console.log(`Key: ${key} Value: ${bookData[key]}`);
+  titleEl.innerText = bookData["name"];
+  authorEl.innerText = bookData["authors"][0];
+  pagesEl.innerText = bookData["numberOfPages"];
+
+  newBookEl.append(titleEl, authorEl, pagesEl);
+
+  booksList.append(newBookEl);
+}
+
+document.addEventListener("DOMContentLoaded", fetchBooks)
