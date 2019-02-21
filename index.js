0e66af0d234da7d0b014b1567f7824019984a9b2
tarasabella <tarasabella@gmail.com>
2019-02-21 10:11:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("hey")
+// })
+
 const testVar = {}
+const bookURL= `https://anapioficeandfire.com/api/books`
+const houseURL = `https://anapioficeandfire.com/api/houses`
+const characterURL = `https://anapioficeandfire.com/api/characters`
 
 function testFunc() {
   return "hi"
+}
+
+fetch(bookURL)
+.then(res => res.json())
+// .then(console.log)
+.then(books => books.forEach(displayBook))
+
+function displayBook(book) {
+console.log(book)
+}
+
+fetch(houseURL)
+.then(res => res.json())
+// .then(console.log)
+.then(houses => houses.forEach(displayHouse))
+
+function displayHouse(house) {
+  console.log(house)
+}
+
+fetch(characterURL)
+.then(res => res.json())
+// .then(console.log)
+.then(characters => characters.forEach(displayCharacter))
+
+function displayCharacter(character) {
+  console.log(character)
 }
