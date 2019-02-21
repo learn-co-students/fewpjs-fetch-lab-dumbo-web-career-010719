43bf8d50a35f8afb59abd1b31ce4aa238703288a
woodylucas <wlucas0311@gmail.com>
2019-02-21 10:08:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const testVar = {}
-
+const body = document.querySelector('body')
+const data = fetchBooks()
 function testFunc() {
   return "hi"
 }
+
+
+const fetchBooks = () => {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+}
+
+const fetchHouses = () => {
+fetch('https://anapioficeandfire.com/api/houses')
+.then(resp => resp.json())
+.then(json => console.log(json));
+}
+
+const fetchCharacters = () => {
+fetch('https://anapioficeandfire.com/api/characters')
+.then(resp => resp.json())
+.then(json => console.log(json));
+}
+
+console.log(fetchBooks());
+console.log(fetchHouses());
+console.log(fetchCharacters());
