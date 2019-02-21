1929c601c87ef50701167ae840ba2ef441836033
Elizabeth Le <elizabethlemk@yahoo.com>
2019-02-21 13:23:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const testVar = {}
-
 function testFunc() {
-  return "hi"
+  return `hi`
 }
+
+
+// const GoTAPIRequest = fetch('https://anapioficeandfire.com/api/book')
+//
+// function fetchBooks() {
+//   .then(function(response) {
+//     return response.json();
+//   })
+//
+// }
