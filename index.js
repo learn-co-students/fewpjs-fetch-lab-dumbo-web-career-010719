253ce241fe2abaf3f66c7e80220cb3a4154fb103
sbrij001 <sbrij001@yahoo.com>
2019-10-02 14:08:09 -0400

    added render books function and fetch books function

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const testVar = {}
+function fetchBooks() {
 
-function testFunc() {
-  return "hi"
 }
+
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
